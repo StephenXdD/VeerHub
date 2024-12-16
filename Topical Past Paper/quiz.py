7deb1974b186796b676ef32396681496fd1dcf0c
Dev Joshi <devnjoshi08@gmail.com>
2024-12-16 13:14:24 +0530

    Delete Topical Past Paper directory

--- Topical Past Paper/quiz.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title("Quiz Tool")
-    st.subheader("WEB TOOL UNDER DEVELOPMENT")
-    st.write("SORRY, COME VISIT LATER")
-
-if __name__ == "__main__":
-    main()
